39afc15bd6e5176a595f8857a1aeecc922a1908b
Pawtetka <45591360+Pawtetka@users.noreply.github.com>
2021-04-27 17:19:28 +0300

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnObject;
+
+    private List<GameObject> objects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNewObject();
+    }
+
+    public void Update()
+    {
+        if (/*Input.GetKeyDown(KeyCode.UpArrow)*/Input.touchCount > 0 || Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            SpawnNewObject();
+        }
+    }
+
+    private void SpawnNewObject()
+    {
+        GameObject gameObject = Instantiate(_spawnObject, new Vector3(Random.Range(-10, 10), 20, 0), Quaternion.identity, this.gameObject.transform);
+        objects.Add(gameObject);
+    }
+}
